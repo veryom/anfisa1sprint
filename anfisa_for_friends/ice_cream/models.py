198017d0dac0361d461454953d880ab16dfdaed2
Viacheslav Eremenko <veryom@gmail.com>
2025-01-06 19:19:45 +0300

    Add relations in models.py in ice_cream app

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 
-# Обёртки
-class Wrapper(models.Model):
+# Категории
+class Category(models.Model):
     title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(default=100)
     is_published = models.BooleanField(default=True)
 
 
@@ -14,17 +16,28 @@ class Topping(models.Model):
     is_published = models.BooleanField(default=True)
 
 
+# Обёртки
+class Wrapper(models.Model):
+    title = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=True)
+
+
 # Сорта мороженного
 class IceCream(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
     is_on_main = models.BooleanField(default=False)
     is_published = models.BooleanField(default=True)
-
-
-# Категории
-class Category(models.Model):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
-    is_published = models.BooleanField(default=True)
+    wrapper = models.OneToOneField(
+        Wrapper,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+    toppings = models.ManyToManyField(
+        Topping
+    )
