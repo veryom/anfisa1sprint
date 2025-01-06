7c62f77c4ccfb7865ed47035df21f7548a49fc13
Viacheslav Eremenko <veryom@gmail.com>
2025-01-06 11:25:25 +0300

    Create models in ice_cream app

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+# Обёртки
+class Wrapper(models.Model):
+    title = models.CharField(max_length=256)
+    is_published = models.BooleanField(default=True)
+
+
+# Топпинги
+class Topping(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    is_published = models.BooleanField(default=True)
+
+
+# Сорта мороженного
+class IceCream(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    is_on_main = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+
+
+# Категории
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(default=100)
+    is_published = models.BooleanField(default=True)
